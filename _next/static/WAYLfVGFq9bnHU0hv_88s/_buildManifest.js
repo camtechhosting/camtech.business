fdf5fc477c32bac13bd87df5b08f9018e8882bb0
Macrofig <Macrofig@users.noreply.github.com>
2023-08-18 06:27:10 +0000

    deploy: c9a55d464e4aecf3a5a14f88abea07843a68d970

---
+++ _next/static/WAYLfVGFq9bnHU0hv_88s/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/db5f3e5593a851d7.css","static/chunks/pages/index-bcbcfbf8427f0105.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
